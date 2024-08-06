8e38c14b74d4341a6ce013b6d8bccf1a696f8591
Harshu2844 <113325223+Harshu2844@users.noreply.github.com>
2024-08-06 06:58:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,8 +60,8 @@ def write_config(config):
 # SCRIPT MADE BY @venomXcrazy ON TELEGRAM THIS IS A PAID SCRIPT MOST POWERFULL A DDOS TELEGRAM BOT SCRIPT
 
 config = load_config()
-bot = telebot.TeleBot(config['bot_token'])
-ADMIN_IDS = set(config['admin_ids'])
+bot = telebot.TeleBot(config['7445553938:AAGDNQUM46KvI5SL1gPRhSbFF_zEEyaJBYE'])
+ADMIN_IDS = set(config['5588464519'])
 USER_FILE = config['user_file']
 LOG_FILE = config['log_file']
 COOLDOWN_TIME = config['cooldown_time']
